4baee6119c437fb3f2316d3eca26a907a0027f2d
sab159 <sab159@duke.edu>
2021-09-21 09:05:54 -0400

    Iterates through all reocrds

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,27 +21,27 @@ line_list = file_object.readlines()
 # Close the file (it's saved into the list)
 file_object.close()
 
-# Read a line of the data
-lineString = line_list[100]
-lineString = line_list[2] #doesn't work - can't be split into data - header rather than data
+# Iterate through all lines in the linelist
+for lineString in line_list: #inserts value of first line into variable 'lineString'
+    
+    # if lineString[0] == "#" or lineString[0] == "u": continue #skips lines that are commented (header)
+    if lineString[0] in ("#", "u"): continue #same as above, but using membership rules
+    
+    # now pull out specific items from that string - split into data items
+    lineData = lineString.split() #creates list object with 18 items
+    
+    #then can use indices to access desired items from each row - extract items into variables
+    # records (columns) of interest: UID, date, location class (quality), lat & long
+    record_id = lineData[0] #record ID
+    obs_date = lineData[2]  #date of observation
+    obs_lc = lineData[4]    #observation location class (quality marker)
+    obs_lat = lineData[6]   #observed latitude
+    obs_long = lineData[7]  #observed longitude
+
+    #print location of Sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long} on {obs_date}")
 
 
-# now pull out specific items from that string - split into data items
-# records (columns) of interest: UID, date, location class (quality), lat & long
-# need string method to pull specific characters from string. Treat as delim.
-lineData = lineString.split() #creates list object with 18 items
-
-#then can use indices to access desired items from each row - extract items into variables
-record_id = lineData[0] #record ID
-obs_date = lineData[2]  #date of observation
-obs_lc = lineData[4]    #observation location class (quality marker)
-obs_lat = lineData[6]   #observed latitude
-obs_long = lineData[7]  #observed longitude
-
-#print location of Sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long} on {obs_date}")
-
-# bring data into code from sara.txt
 
 
 
