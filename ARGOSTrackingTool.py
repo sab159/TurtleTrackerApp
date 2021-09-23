086ba986084a91848ad1d1b0185569816894d68c
sab159 <sab159@duke.edu>
2021-09-23 11:44:51 -0400

    Displays location for user date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,6 +9,9 @@
 # Date:   Fall 2021
 #--------------------------------------------------------------
 
+#Solicit search date from user
+user_date = input("Enter date (M/D/YYYY) to search for Sara: ")
+
 # Create a variable pointing to the data file
 file_name = './data/raw/sara.txt'
 
@@ -47,14 +50,31 @@ for lineString in line_list: #inserts value of first line into variable 'lineStr
         continue                      #skips to next iteration if value is not among acceptable values
     
     #print location of Sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long} on {obs_date}")
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long} on {obs_date}")
     
     #add items to dictionaries
     date_dict[record_id] = obs_date #UID used as key, date passed as value 
     coord_dict[record_id] = (obs_lat, obs_long) #UID as key, lat/long pair as value
 
+#Create empty list to store keys corresponding with user date
+matching_keys=[]   
+    
+#Loop through items in the date_dict, and collect keys for matching ones
+for date_item in date_dict.items():
+    
+    #Get the key and date components of dictionary item
+    the_key, the_date = date_item #sepatates tuple into two values
+    
+    #Check if the date matches the user-specified date
+    if the_date == user_date:
+        #if so, add key to the list
+        matching_keys.append(the_key)
+        
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys: 
+    obs_lat, obs_long = coord_dict[matching_key] #split tuple into two variables
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat}, lon:{obs_long} on {user_date}")
 
-
-
+    
 
 
